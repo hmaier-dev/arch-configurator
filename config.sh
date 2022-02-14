930a5e3ddb6a8a4f43fe8ebd1606180e0d2cdcc2
hmaier <hmaier@ipb-halle.de>
2022-02-14 14:52:43 +0100

    Installing packages

--- config.sh
+++ config.sh
@@ -14,22 +14,18 @@ p () {
 }
 
 read -p "Enter local username: " username
-echo -e "\n"
 
 read -p "Enter preferred user id [1001]: " userid
 userid=${userid:-1001}
-echo -e "\n"
 
 read -p "Enter domain username: " uname
 read -s -p "Enter domain password: " password
 
 echo -e "Creating User..."
 useradd -m --uid 1001 -G wheel $username
-echo -e "\n"
 
 echo -e "Changing password of $username"
 passwd $username
-echo -e "\n"
 
 echo -e "Creating networkshare directorys..."
 
@@ -43,14 +39,22 @@ p "cifs-utils"
 mount.cifs //isilon/Member_Home	/home/$username/networkshare/$uname -o pass=$password,user=$uname,uid=$userid,gid=$userid
 
 if [ -d "/home/$username/networkshare/$uname/linux-files" ]; then
-	echo -e "Successful connection to the networkshare!"
+	echo -e "Successful connection to private networkshare!"
 	mkdir -p /home/$username/networkshare/IPB
 	mount.cifs //isilon/IPB	/home/$username/networkshare/IPB -o pass=$password,user=$uname,uid=$userid,gid=$userid
 	mkdir -p /home/$username/networkshare/AdmIn
 	mount.cifs //isilon/workspace /home/$username/networkshare/AdmIn -o pass=$password,user=$uname,uid=$userid,gid=$userid
-
+	echo -e "Connected to public networkshares."	
 else
 	echo -e "No connection to the networkshare."
 	exit
 fi
 
+echo -e "Downloading packages..."
+# take care: this will also read empty lines!
+while read -r i; do
+	p "$i"
+done < /home/$username/networkshare/$uname/linux-files/packages-list.txt
+
+
+echo -e "Copying config files..."
