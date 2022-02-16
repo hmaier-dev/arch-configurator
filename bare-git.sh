370a3b5b76d0210bdb03721b3836b2a86a315f21
hmaier <hmaier@ipb-halle.de>
2022-02-16 14:46:10 +0100

    solved: added git checkout

--- bare-git.sh
+++ bare-git.sh
@@ -3,6 +3,7 @@
 echo -e "--------------------------------------------------------------"
 echo -e "This script will setup a bare git repo, for managing dotfiles."
 echo -e "Don't run this as root."
+echo -e "Also beware: this will overwrite existing dotfiles!"
 echo -e "--------------------------------------------------------------"
 
 read -e -p "Continue with the script? [y/n/q]" PROCEED 
@@ -10,11 +11,30 @@ PROCCED=${PROCEED:-n}
 [ $PROCCED != "y" ] && exit
 
 u=$USER
+
+echo -e "Searching for public ssh-key..."
+while [ ! -f /home/$u/.ssh/id_rsa.pub ]; do
+	ssh-keygen
+done
+
+echo -e "Writing your public ssh-key to networkshare..."
+cp /home/$u/.ssh/id_rsa.pub /home/$u/networkshare/hmaier/linux-files/public_ssh_key.txt
+
+read -e -p "Have you copied your public ssh-key to github? [y/n/q]" PROCEED 
+PROCCED=${PROCEED:-n}
+[ $PROCCED != "y" ] && echo -e "Go do that!" && exit
+
+read -p "Enter your email (for git): " email
+read -p "Enter your email (for git): " name
+
+git config --global user.email "$email"
+git config --global user.name "$name"
+
+#deleting generated bashrc to prevent errors
+rm $HOME/.bashrc
+
 echo -e "Creating dotfile folder.."
 mkdir -p /home/$u/repos/dotfiles
-
-echo -e "Echoing dfr-alias to the bashrc..."
-echo "alias dfr='/usr/bin/git --git-dir=$HOME/repos/dotfiles --working-tree=$HOME'" >> ~/.bashrc
 
 echo "repos/dotfiles" >> .gitignore
 
@@ -25,12 +45,8 @@ git clone --bare git@github.com:hmaier-ipb/dotfiles.git $HOME/repos/dotfiles
 /usr/bin/git --git-dir=$HOME/repos/dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no
 /usr/bin/git --git-dir=$HOME/repos/dotfiles/ --work-tree=$HOME checkout
 
-echo -e "Searching for public ssh-key..."
-while [ ! -f /home/$u/.ssh/id_rsa.pub ]; do
-	ssh-keygen
-done
 
-echo -e "Writing your public ssh-key to networkshare..."
-cp /home/$u/.ssh/id_rsa.pub /home/$u/networkshare/hmaier/linux-files/public_ssh_key.txt
-
+echo -e "When tracking file into the dotfiles repo, use the alias dfr instead of git."
+echo -e "git -> dfr"
 echo -e "Thank you for organizing dotfiles!"
+
