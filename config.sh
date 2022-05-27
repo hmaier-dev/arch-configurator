5a906c67836b45219e3861841548c8fb9d1d8027
Hendrik Maier <hmaier@ipb-halle.de>
2022-05-27 08:18:48 +0200

    geändert:       config.sh

--- config.sh
+++ config.sh
@@ -140,6 +140,18 @@ sed -i "/#greeter-session=/c\greeter-session=lightdm-gtk-greeter" /etc/lightdm/l
 sed -i "/#display-setup-script=/c\display-setup-script=/usr/bin/setxkbmap de" /etc/lightdm/lightdm.conf
 #sed -i "/#greeter-setup-script=/c\greeter-setup-script=/usr/bin/numlockx on" /etc/lightdm/lightdm.conf
 
+cp /usr/share/pixmaps/archlinux-logo.png /etc/lightdm
+if [ -f /etc/lightdm/lightdm-gtk-greeter.conf ];then
+	rm /etc/lightdm/lightdm-gtk-greeter.conf
+fi
+
+touch /etc/lightdm/lightdm-gtk-greeter.conf
+echo "[greeter]" >> /etc/lightdm/lightdm-gtk-greeter.conf
+echo "background = #5e5c64 " >> /etc/lightdm/lightdm-gtk-greeter.conf
+echo "default-user-image = /etc/lightdm/archlinux-logo.png" >> /etc/lightdm/lightdm-gtk-greeter.conf
+
+
+
 echo -e "\n"
 echo -e "Now you can log in as $username!"
 echo -e "For syncing your dotfiles, run ./sync-dotfiles.sh as $username."
