8eff7be8d7ed4ed539b46977122c751dab074a17
Hendrik Maier <hmaier@ipb-halle.de>
2022-08-31 16:47:27 +0200

    separated networkshare configuartion for work and home

--- config.sh
+++ config.sh
@@ -3,7 +3,7 @@
 echo -e "------------------------------------------------------------"
 echo -e "Welcome to self-configuring Arch!"
 echo -e "Run this script only on fresh installed systems."
-echo -e "It will install my dotfiles as well as config file for:"
+echo -e "It will install my dotfiles as well as config files for:"
 echo -e "bspwm,sxhkd,neovim"
 echo -e "------------------------------------------------------------\n"
 
@@ -18,11 +18,105 @@ p () {
 	echo -e "Installing package: $1"
 	pacman --noconfirm --needed -S $1 >/dev/null 2>&1
 }
+connect_isilon () {
+	echo -e "For access to the isilon domain credentials are needed."	
+	read -p "Enter domain username: " uname
+	read -s -p "Enter domain password: " password
+	echo -e "\nCreating a credentials file..."
+	touch /home/$username/.isilon_access
+	echo "username=$uname" >> /home/$username/.isilon_access
+	echo "pass=$password" >> /home/$username/.isilon_access
+	chmod 600 /home/$username/.isilon_access # just root can read/write this file
+	echo -e "Creating networkshare directorys..."
+	mkdir -p /home/$username/networkshare
+	mkdir -p /home/$username/networkshare/$uname
+	echo -e "Connection to networkshare..."
+	# for mounting the networkshare, this package is needed
+	p "cifs-utils"
+
+	mount.cifs //isilon/Member_Home	/home/$username/networkshare/$uname -o pass=$password,user=$uname,uid=$userid,gid=$userid
+
+	if [ -d "/home/$username/networkshare/$uname/linux-files" ]; then
+		echo -e "Successful connection to private networkshare!"
+		mkdir -p /home/$username/networkshare/IPB
+		mount.cifs //isilon/IPB	/home/$username/networkshare/IPB -o pass=$password,user=$uname,uid=$userid,gid=$userid
+		mkdir -p /home/$username/networkshare/AdmIn
+		mount.cifs //isilon/workspace /home/$username/networkshare/AdmIn -o pass=$password,user=$uname,uid=$userid,gid=$userid
+		echo -e "Connected to public networkshares."	
+	else
+		echo -e "No connection to the networkshare. Delete line 65 to continue."
+		exit
+	fi
+
+	echo -e "Copy unit-files for systemd-automount..."
+
+	# copying all mount and automount units
+	cp -r /home/$username/networkshare/$uname/linux-files/mounting-with-systemd /root
+	cp /root/mounting-with-systemd/* /etc/systemd/system 
+
+	#for network-online.target this unit has to be enabled
+	systemctl enable systemd-networkd.service --quiet
+	systemctl start systemd-networkd.service --quiet
+	# change the unit files "options=" individually?
+	cd /root/mounting-with-systemd/
+
+	for unit in *.automount;do
+		echo -e "Enabling service for $unit."
+		systemctl enable $unit --quiet
+	#	systemctl start $unit
+	done
+
+}
+
+connect_fritz_nas () {
+	echo -e "For connection the fritz nas your credentials are needed."
+	read -p "Enter nas username: " uname
+	read -s -p "Enter nas password: " password
+
+	echo -e "\nCreating a credentials file..."
+	touch /home/$username/.smbcredentials
+	echo "username=$uname" >> /home/$username/.smbcredentials
+	echo "pass=$password" >> /home/$username/.smbcredentials
+	chmod 600 /home/$username/.isilon_access # just root can read/write this file
+
+	echo -e "Creating networkshare directorys..."
+	mkdir -p /home/$username/networkshare
+
+	p "cifs-utils"
+
+	echo -e "Creating custom mount-units..."
+	touch /etc/systemd/system/home-$username-networkshare.mount
+	touch /etc/systemd/system/home-$username-networkshare.automount
+	echo -e "Writing home-$username-networkshare.mount"
+	echo -e "[Unit]\n" >> /etc/systemd/system/home-$username-networkshare.mount
+	echo -e "\n" >> /etc/systemd/system/home-$username-networkshare.mount
+	echo -e "Description=the fritz nas on our wlan-repeater\n" >> /etc/systemd/system/home-$username-networkshare.mount
+	echo -e "Wants=network-online.target\n" >> /etc/systemd/system/home-$username-networkshare.mount
+	echo -e "After=network-online.target\n" >> /etc/systemd/system/home-$username-networkshare.mount
+	echo -e "\n" >> /etc/systemd/system/home-$username-networkshare.mount
+	echo -e "[Mount]\n" >> /etc/systemd/system/home-$username-networkshare.mount
+	echo -e "What=//192.168.178.99/nas/private_hdd\n" >> /etc/systemd/system/home-$username-networkshare.mount
+	echo -e "Where=/home/$username/networkshare\n" >> /etc/systemd/system/home-$username-networkshare.mount
+	echo -e "Type=cifs\n" >> /etc/systemd/system/home-$username-networkshare.mount
+	echo -e "Options=credentials=/home/hmaier/.smbcredentials,vers=2.1,noserverino,uid=1001,gid=1001\n" >> /etc/systemd/system/home-$username-networkshare.mount
+	echo -e "\n" >> /etc/systemd/system/home-$username-networkshare.mount
+	echo -e "[Install]\n" >> /etc/systemd/system/home-$username-networkshare.mount
+	echo -e "WantedBy=multi-user.target\n" >> /etc/systemd/system/home-$username-networkshare.mount
+
+	echo -e "Writing home-$username-networkshare.automount"
+	echo -e "[Unit]\n" >> /etc/systemd/system/home-$username-networkshare.automount
+	echo -e "Description=Automount networkshare to home directory\n" >> /etc/systemd/system/home-$username-networkshare.automount
+	echo -e "\n" >> /etc/systemd/system/home-$username-networkshare.automount
+	echo -e "[Automount]\n" >> /etc/systemd/system/home-$username-networkshare.automount
+	echo -e "Where=/home/hmaier/networkshare\n" >> /etc/systemd/system/home-$username-networkshare.automount
+	echo -e "\n" >> /etc/systemd/system/home-$username-networkshare.automount
+	echo -e "[Install]\n" >> /etc/systemd/system/home-$username-networkshare.automount
+	echo -e "WantedBy=multi-user.target\n" >> /etc/systemd/system/home-$username-networkshare.automount
+	
+}
 
 read -p "Enter the hostname for this device: " hostname
-
 read -p "Enter local username: " username
-
 read -p "Enter preferred user id [1001]: " userid
 userid=${userid:-1001}
 
@@ -32,56 +126,14 @@ useradd -m --uid 1001 -G wheel $username
 echo -e "Changing password of $username"
 passwd $username
 
-read -p "Enter domain username: " uname
-read -s -p "Enter domain password: " password
-
-
-echo -e "\nCreating a credentials file..."
-touch /home/$username/.isilon_access
-echo "username=$uname" >> /home/$username/.isilon_access
-echo "pass=$password" >> /home/$username/.isilon_access
-chmod 600 /home/$username/.isilon_access # just root can read/write this file
-
-echo -e "Creating networkshare directorys..."
-
-mkdir -p /home/$username/networkshare
-mkdir -p /home/$username/networkshare/$uname
-
-echo -e "Connection to networkshare..."
-# for mounting the networkshare, this package is needed
-p "cifs-utils"
-
-mount.cifs //isilon/Member_Home	/home/$username/networkshare/$uname -o pass=$password,user=$uname,uid=$userid,gid=$userid
-
-if [ -d "/home/$username/networkshare/$uname/linux-files" ]; then
-	echo -e "Successful connection to private networkshare!"
-	mkdir -p /home/$username/networkshare/IPB
-	mount.cifs //isilon/IPB	/home/$username/networkshare/IPB -o pass=$password,user=$uname,uid=$userid,gid=$userid
-	mkdir -p /home/$username/networkshare/AdmIn
-	mount.cifs //isilon/workspace /home/$username/networkshare/AdmIn -o pass=$password,user=$uname,uid=$userid,gid=$userid
-	echo -e "Connected to public networkshares."	
-else
-	echo -e "No connection to the networkshare. Delete line 65 to continue."
-	exit
+read -e -p "Are you at [h]ome or at [w]ork? [h/w/n/q]" PROCEED 
+PROCCED=${PROCEED:-n}
+if [[ $PROCCED = "w" ]]; then
+	connect_isilon
 fi
-
-echo -e "Copy unit-files for systemd-automount..."
-
-# copying all mount and automount units
-cp -r /home/$username/networkshare/$uname/linux-files/mounting-with-systemd /root
-cp /root/mounting-with-systemd/* /etc/systemd/system 
-
-#for network-online.target this unit has to be enabled
-systemctl enable systemd-networkd.service --quiet
-systemctl start systemd-networkd.service --quiet
-# change the unit files "options=" individually?
-cd /root/mounting-with-systemd/
-
-for unit in *.automount;do
-	echo -e "Enabling service for $unit."
-	systemctl enable $unit --quiet
-#	systemctl start $unit
-done
+if [[ $PROCCED = "h"  ]]; then
+	connect_fritz_nas
+fi
 
 echo -e "Copying script for dotfiles management..."
 cd /root
@@ -96,7 +148,6 @@ cd /root
 cp /root/arch-configurator/workspace-setup.sh /home/$username
 chown $username /home/$username/workspace-setup.sh
 chmod +x /home/$username/workspace-setup.sh
-
 
 echo -e "Base Configuration finished."
 
@@ -135,19 +186,18 @@ rmmod pcspkr
 echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf
 
 echo -e "Creating work directorys..."
-mkdir /home/$username/Dokumente
-chown $username /home/$username/Dokumente
-mkdir /home/$username/Bilder
-chown $username /home/$username/Bilder
-mkdir /home/$username/Bilder/screenshots
-chown $username /home/$username/Bilder/screenshots
-
+mkdir /home/$username/pics
+chown $username /home/$username/pics
+mkdir /home/$username/pics/screenshots
+chown $username /home/$username/pics/screenshots
 
 echo -e "Enabling lightdm display manager..."
 systemctl enable lightdm --quiet
 sed -i "/#greeter-session=/c\greeter-session=lightdm-gtk-greeter" /etc/lightdm/lightdm.conf
 sed -i "/#display-setup-script=/c\display-setup-script=/usr/bin/setxkbmap de" /etc/lightdm/lightdm.conf
 #sed -i "/#greeter-setup-script=/c\greeter-setup-script=/usr/bin/numlockx on" /etc/lightdm/lightdm.conf
+
+echo -e "Configuring lightdm..."
 
 cp /usr/share/pixmaps/archlinux-logo.png /etc/lightdm
 if [ -f /etc/lightdm/lightdm-gtk-greeter.conf ];then
